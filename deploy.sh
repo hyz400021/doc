6f149ae5e1541e5b94a2e1415ab8a25b76ad0e6a
hyz400021 <xox@qq.com>
2023-12-30 19:30:49 +0800

    初始化20

--- deploy.sh
+++ deploy.sh
@@ -6,14 +6,15 @@ cd docs/.vuepress/dist # 进入生成的文件夹
 
 # deploy to github
 
+git config --global user.name "黄元治"
+git config --global user.email "xox@qq.com"
+
 if [ -z "$GITHUB_TOKEN" ]; then
   msg='deploy'
   githubUrl=git@github.com:hyz400021/doc.git
 else
   msg='来自github action的自动部署'
   githubUrl=https://${GITHUB_TOKEN}@github.com/hyz400021/doc.git
-  git config --global user.name "黄元治"
-  git config --global user.email "xox@qq.com"
 fi
 git init
 git add -A
